e6b9a83939e749c37236438a19cba94bd4b30a68
MAC <mac@MACs-MacBook-Pro.local>
2023-03-15 13:48:19 +0600

    first commit

---
+++ core/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.views import View
+from .models import Student
+
+
+# Create your views here.
+
+
+class Home(View):
+   def get(self, request):
+       stu_data = Student.objects.all()
+       return render(request, 'core/home.html', {'studata':stu_data})
